e3fe9ecbcb8a45cf3eb725dffea7dd2a271058e6
anirudh094 <anirudhgarg094@gmail.com>
2024-03-01 15:57:20 +0530

    Posts Added

--- src/Pages/Create.js
+++ src/Pages/Create.js
@@ -1,10 +1,27 @@
-import React from 'react'
-import "../CSS/Create.css"
+import React from "react";
+import "../CSS/Create.css";
+import { Icon } from "semantic-ui-react";
 
 function Create() {
   return (
-    <div className='create-body'>Create</div>
-  )
+    <div className="create-body">
+      <div className="create-post">
+        <img
+          className="create-post-dp"
+          src="https://media.istockphoto.com/id/1309328823/photo/headshot-portrait-of-smiling-male-employee-in-office.jpg?b=1&s=612x612&w=0&k=20&c=eU56mZTN4ZXYDJ2SR2DFcQahxEnIl3CiqpP3SOQVbbI="
+          alt="profile"
+        ></img>
+        <div className="create-post-caption">What's on your mind, Name?</div>
+      </div>
+      <div className="create-post-addon">
+        <div className="create-post-image">
+          <Icon name="image" size="large" color="orange" />
+          <div>Media</div>
+        </div>
+        <button className="create-post-btn">Share</button>
+      </div>
+    </div>
+  );
 }
 
-export default Create
+export default Create;
--- src/Pages/Explore.js
+++ src/Pages/Explore.js
@@ -58,6 +58,266 @@ function Explore() {
             />
           </div>
         </div>
+        <div className="explore-body-cards">
+          <div className="explore-body-cards-acc-info">
+            <img
+              src="https://media.istockphoto.com/id/1309328823/photo/headshot-portrait-of-smiling-male-employee-in-office.jpg?b=1&s=612x612&w=0&k=20&c=eU56mZTN4ZXYDJ2SR2DFcQahxEnIl3CiqpP3SOQVbbI="
+              alt="profile"
+            ></img>
+            <div>
+              <h4>cameron_will</h4>
+              <h5>Mumbai, India</h5>
+            </div>
+          </div>
+          <img
+            className="post-image"
+            src="https://img.freepik.com/free-photo/painting-mountain-lake-with-mountain-background_188544-9126.jpg?size=626&ext=jpg&ga=GA1.1.1395880969.1709164800&semt=sph"
+            alt="post"
+          ></img>
+          <div className="explore-post-impressions">
+            <Icon
+              className="explore-post-impressions-icon"
+              name="heart"
+              size="large"
+              color="red"
+            />
+            <Icon
+              className="explore-post-impressions-icon"
+              name="comment"
+              size="large"
+              color="grey"
+            />
+            <Icon
+              className="explore-post-impressions-icon"
+              name="share alternate"
+              size="large"
+              color="grey"
+            />
+          </div>
+          <div className="explore-post-caption">
+            <Icon
+              className="explore-post-caption-quotes"
+              name="quote left"
+              size="small"
+              color="white"
+            />
+            FaceGram is better than instabook
+            <Icon
+              className="explore-post-caption-quotes"
+              name="quote right"
+              size="small"
+              color="white"
+            />
+          </div>
+        </div>
+        <div className="explore-body-cards">
+          <div className="explore-body-cards-acc-info">
+            <img
+              src="https://media.istockphoto.com/id/1309328823/photo/headshot-portrait-of-smiling-male-employee-in-office.jpg?b=1&s=612x612&w=0&k=20&c=eU56mZTN4ZXYDJ2SR2DFcQahxEnIl3CiqpP3SOQVbbI="
+              alt="profile"
+            ></img>
+            <div>
+              <h4>cameron_will</h4>
+              <h5>Mumbai, India</h5>
+            </div>
+          </div>
+          <img
+            className="post-image"
+            src="https://img.freepik.com/free-photo/painting-mountain-lake-with-mountain-background_188544-9126.jpg?size=626&ext=jpg&ga=GA1.1.1395880969.1709164800&semt=sph"
+            alt="post"
+          ></img>
+          <div className="explore-post-impressions">
+            <Icon
+              className="explore-post-impressions-icon"
+              name="heart"
+              size="large"
+              color="red"
+            />
+            <Icon
+              className="explore-post-impressions-icon"
+              name="comment"
+              size="large"
+              color="grey"
+            />
+            <Icon
+              className="explore-post-impressions-icon"
+              name="share alternate"
+              size="large"
+              color="grey"
+            />
+          </div>
+          <div className="explore-post-caption">
+            <Icon
+              className="explore-post-caption-quotes"
+              name="quote left"
+              size="small"
+              color="white"
+            />
+            FaceGram is better than instabook
+            <Icon
+              className="explore-post-caption-quotes"
+              name="quote right"
+              size="small"
+              color="white"
+            />
+          </div>
+        </div>
+        <div className="explore-body-cards">
+          <div className="explore-body-cards-acc-info">
+            <img
+              src="https://media.istockphoto.com/id/1309328823/photo/headshot-portrait-of-smiling-male-employee-in-office.jpg?b=1&s=612x612&w=0&k=20&c=eU56mZTN4ZXYDJ2SR2DFcQahxEnIl3CiqpP3SOQVbbI="
+              alt="profile"
+            ></img>
+            <div>
+              <h4>cameron_will</h4>
+              <h5>Mumbai, India</h5>
+            </div>
+          </div>
+          <img
+            className="post-image"
+            src="https://img.freepik.com/free-photo/painting-mountain-lake-with-mountain-background_188544-9126.jpg?size=626&ext=jpg&ga=GA1.1.1395880969.1709164800&semt=sph"
+            alt="post"
+          ></img>
+          <div className="explore-post-impressions">
+            <Icon
+              className="explore-post-impressions-icon"
+              name="heart"
+              size="large"
+              color="red"
+            />
+            <Icon
+              className="explore-post-impressions-icon"
+              name="comment"
+              size="large"
+              color="grey"
+            />
+            <Icon
+              className="explore-post-impressions-icon"
+              name="share alternate"
+              size="large"
+              color="grey"
+            />
+          </div>
+          <div className="explore-post-caption">
+            <Icon
+              className="explore-post-caption-quotes"
+              name="quote left"
+              size="small"
+              color="white"
+            />
+            FaceGram is better than instabook
+            <Icon
+              className="explore-post-caption-quotes"
+              name="quote right"
+              size="small"
+              color="white"
+            />
+          </div>
+        </div>
+        <div className="explore-body-cards">
+          <div className="explore-body-cards-acc-info">
+            <img
+              src="https://media.istockphoto.com/id/1309328823/photo/headshot-portrait-of-smiling-male-employee-in-office.jpg?b=1&s=612x612&w=0&k=20&c=eU56mZTN4ZXYDJ2SR2DFcQahxEnIl3CiqpP3SOQVbbI="
+              alt="profile"
+            ></img>
+            <div>
+              <h4>cameron_will</h4>
+              <h5>Mumbai, India</h5>
+            </div>
+          </div>
+          <img
+            className="post-image"
+            src="https://img.freepik.com/free-photo/painting-mountain-lake-with-mountain-background_188544-9126.jpg?size=626&ext=jpg&ga=GA1.1.1395880969.1709164800&semt=sph"
+            alt="post"
+          ></img>
+          <div className="explore-post-impressions">
+            <Icon
+              className="explore-post-impressions-icon"
+              name="heart"
+              size="large"
+              color="red"
+            />
+            <Icon
+              className="explore-post-impressions-icon"
+              name="comment"
+              size="large"
+              color="grey"
+            />
+            <Icon
+              className="explore-post-impressions-icon"
+              name="share alternate"
+              size="large"
+              color="grey"
+            />
+          </div>
+          <div className="explore-post-caption">
+            <Icon
+              className="explore-post-caption-quotes"
+              name="quote left"
+              size="small"
+              color="white"
+            />
+            FaceGram is better than instabook
+            <Icon
+              className="explore-post-caption-quotes"
+              name="quote right"
+              size="small"
+              color="white"
+            />
+          </div>
+        </div>
+        <div className="explore-body-cards">
+          <div className="explore-body-cards-acc-info">
+            <img
+              src="https://media.istockphoto.com/id/1309328823/photo/headshot-portrait-of-smiling-male-employee-in-office.jpg?b=1&s=612x612&w=0&k=20&c=eU56mZTN4ZXYDJ2SR2DFcQahxEnIl3CiqpP3SOQVbbI="
+              alt="profile"
+            ></img>
+            <div>
+              <h4>cameron_will</h4>
+              <h5>Mumbai, India</h5>
+            </div>
+          </div>
+          <img
+            className="post-image"
+            src="https://img.freepik.com/free-photo/painting-mountain-lake-with-mountain-background_188544-9126.jpg?size=626&ext=jpg&ga=GA1.1.1395880969.1709164800&semt=sph"
+            alt="post"
+          ></img>
+          <div className="explore-post-impressions">
+            <Icon
+              className="explore-post-impressions-icon"
+              name="heart"
+              size="large"
+              color="red"
+            />
+            <Icon
+              className="explore-post-impressions-icon"
+              name="comment"
+              size="large"
+              color="grey"
+            />
+            <Icon
+              className="explore-post-impressions-icon"
+              name="share alternate"
+              size="large"
+              color="grey"
+            />
+          </div>
+          <div className="explore-post-caption">
+            <Icon
+              className="explore-post-caption-quotes"
+              name="quote left"
+              size="small"
+              color="white"
+            />
+            FaceGram is better than instabook
+            <Icon
+              className="explore-post-caption-quotes"
+              name="quote right"
+              size="small"
+              color="white"
+            />
+          </div>
+        </div>
       </div>
     </>
   );
