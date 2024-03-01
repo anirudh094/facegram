bbbda9801356e4d9613c1d83d480ea1ae7b5eafd
anirudh094 <anirudhgarg094@gmail.com>
2024-03-02 02:24:34 +0530

    Responsive done

--- src/Pages/SideBar.js
+++ src/Pages/SideBar.js
@@ -3,14 +3,19 @@ import "../CSS/SideBar.css";
 import Profile from "./Profile";
 import Navigation from "./Navigation";
 import Logout from "./Logout";
+
 function SideBar() {
   return (
-    <div className="sidebar-body">
-      <img className="logo" src="images/logo.png" alt="logo"></img>
-      <Profile/>
-      <Navigation/>
-      <Logout/>
-    </div>
+    <>
+      {" "}
+      <div className="sidebar-body">
+        <div className="side-bar-big">
+          <Profile />
+          <Navigation />
+          <Logout />
+        </div>
+      </div>
+    </>
   );
 }
 
--- src/Pages/TopBar.js
+++ src/Pages/TopBar.js
@@ -1,9 +1,53 @@
 import React from "react";
 import "../CSS/TopBar.css";
+import Profile from "./Profile";
+import Navigation from "./Navigation";
+import Logout from "./Logout";
+import MenuIcon from "@rsuite/icons/Menu";
+import { Drawer, ButtonToolbar, Button } from "rsuite";
+
 function TopBar() {
+  const MenuUIcon = ({ size }) => (
+    <MenuIcon style={{ fontSize: size, marginRight: 10, color: "white" }} />
+  );
+  const [size, setSize] = React.useState();
+  const [open, setOpen] = React.useState(false);
+  const handleOpen = (value) => {
+    setSize(value);
+    setOpen(true);
+  };
+
   return (
     <div className="topbar-body">
+      <div className="logo">
+        <img src="images/logo.png" alt="logo"></img>
+      </div>
+      <div className="topbar-body-box">
         <div className="search-box">Search</div>
+      </div>
+      <div>
+        <ButtonToolbar className="menu-btn">
+          <Button onClick={() => handleOpen("300px")}>
+            <span>
+              <MenuUIcon size="30px" />
+            </span>
+          </Button>
+        </ButtonToolbar>
+
+        <Drawer
+          size={size}
+          open={open}
+          onClose={() => setOpen(false)}
+          placement="left"
+        >
+          <Drawer.Body>
+            <Profile />
+            <Navigation />
+            <Logout />
+            <div className="search-box search-box-drawer">Search</div>
+          </Drawer.Body>
+        </Drawer>
+      </div>
     </div>
   );
 }
